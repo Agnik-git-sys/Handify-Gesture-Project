2030551d44fef2682fde1c6ad92dfa8da5e35a84
Agnik <link.agnik@gmail.com>
2025-12-11 01:05:15 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import cv2
+def camera():
+    cam = cv2.VideoCapture(0)
+
+    while True:
+        ret, frame = cam.read()
+
+        if not ret:
+            print("cam not working")
+            break
+        cv2.imshow("agnik",frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        
+    cam.release()
+    cv2.destroyAllWindows()
+            
+
+
+if __name__ == "__main__":
+    camera()
